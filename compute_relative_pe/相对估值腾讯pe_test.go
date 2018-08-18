2d53e7e5fe0965d56af293f0c18b668476b29ebc
sionnow0 <sionnow0@gmail.com>
2018-08-18 15:29:33 +0800

    调整了一遍相对估值法

--- compute_relative_pe/相对估值腾讯pe_test.go
+++ compute_relative_pe/相对估值腾讯pe_test.go
@@ -28,39 +28,6 @@ func Test_pe_v1(t *testing.T) {
 	net_profit_2021 := compute_to_2021(1.3) // 这里算了成长4年，比dcf多算了一年
 	r := 0.867      // 港币/人民币
 	// 此刻20180712市值为3.61万亿港币，总股本95.05亿
-	var pe_range_l, pe_range_r float64 = 25, 30
-
-	// ###########################
-	a_2021_25 := net_profit_2021 * pe_range_l / r
-	//fmt.Printf("2021 25pe: %.0fHKD\n", a_2021_25)
-	a_2021_30 := net_profit_2021 * pe_range_r / r
-
-	hard_code := uint32(310)
-	if round(a_2021_25/G_All_Stock_Num/2) == hard_code {
-		t.Logf("2017 can buy price { %dpe: %d/%d HKD }", round(pe_range_l), hard_code*2, hard_code)
-	} else {
-		t.Errorf("2017 can buy price { %dpe: 248 HKD, but:%d }", round(pe_range_l), round(a_2021_25/G_All_Stock_Num/2))
-	}
-	hard_code1 := uint32(372)
-	if round(a_2021_30/G_All_Stock_Num/2) == hard_code1 {
-		t.Logf("2017 can buy price { %dpe: %d/%d HKD }", round(pe_range_r), hard_code1*2, hard_code1)
-	} else {
-		t.Errorf("2017 can buy price { %dpe: 372 HKD, but:%d }", round(pe_range_r), round(a_2021_30/G_All_Stock_Num/2))
-	}
-
-	year_num := 4.0
-	t.Logf("公司30%%增长，收益增长率范围为%.1f%% ~ %.1f%%",
-		utils.GetRatio(G_My_Costing, 620, year_num),
-		utils.GetRatio(G_My_Costing, 744, year_num))
-}
-
-func Test_pe_v2(t *testing.T) {
-	// ###########################
-
-	// 715是2017年股东应占溢利
-	net_profit_2021 := compute_to_2021(1.3) // 这里算了成长4年，比dcf多算了一年
-	r := 0.867      // 港币/人民币
-	// 此刻20180712市值为3.61万亿港币，总股本95.05亿
 	var pe_range_l, pe_range_r float64 = 20, 30
 
 	// ###########################
@@ -87,7 +54,73 @@ func Test_pe_v2(t *testing.T) {
 		utils.GetRatio(G_My_Costing, 744, year_num))
 }
 
+func Test_pe_v2(t *testing.T) {
+	// ###########################
+
+	// 715是2017年股东应占溢利
+	net_profit_2021 := compute_to_2021(1.3) // 这里算了成长4年，比dcf多算了一年
+	r := 0.867      // 港币/人民币
+	// 此刻20180712市值为3.61万亿港币，总股本95.05亿
+	var pe_range_l, pe_range_r float64 = 25, 30
+
+	// ###########################
+	a_2021_25 := net_profit_2021 * pe_range_l / r
+	//fmt.Printf("2021 25pe: %.0fHKD\n", a_2021_25)
+	a_2021_30 := net_profit_2021 * pe_range_r / r
+
+	hard_code := uint32(310)
+	if round(a_2021_25/G_All_Stock_Num/2) == hard_code {
+		t.Logf("2017 can buy price { %dpe: %d/%d HKD }", round(pe_range_l), hard_code*2, hard_code)
+	} else {
+		t.Errorf("2017 can buy price { %dpe: 248 HKD, but:%d }", round(pe_range_l), round(a_2021_25/G_All_Stock_Num/2))
+	}
+	hard_code1 := uint32(372)
+	if round(a_2021_30/G_All_Stock_Num/2) == hard_code1 {
+		t.Logf("2017 can buy price { %dpe: %d/%d HKD }", round(pe_range_r), hard_code1*2, hard_code1)
+	} else {
+		t.Errorf("2017 can buy price { %dpe: 372 HKD, but:%d }", round(pe_range_r), round(a_2021_30/G_All_Stock_Num/2))
+	}
+
+	year_num := 4.0
+	t.Logf("公司30%%增长，收益增长率范围为%.1f%% ~ %.1f%%",
+		utils.GetRatio(G_My_Costing, 620, year_num),
+		utils.GetRatio(G_My_Costing, 744, year_num))
+}
+
 func Test_pe_v3(t *testing.T) {
+
+	// ###########################
+	// 715是2017年股东应占溢利
+	net_profit_2021 := compute_to_2021(1.2) // 这里算了成长4年，比dcf多算了一年
+	r := 0.867      // 港币/人民币
+	// 此刻20180712市值为3.61万亿港币，总股本95.05亿
+	var pe_range_l, pe_range_r float64 = 20, 30
+	// ###########################
+
+	a_2021_25 := net_profit_2021 * pe_range_l / r
+
+	a_2021_30 := net_profit_2021 * pe_range_r / r
+
+	hard_code := uint32(180)
+	if round(a_2021_25/G_All_Stock_Num/2) == hard_code {
+		t.Logf("2017 can buy price { %dpe: %d/%d HKD }", round(pe_range_l), hard_code*2, hard_code)
+	} else {
+		t.Errorf("2017 can buy price { %dpe: 248 HKD, but:%d }", round(pe_range_l), round(a_2021_25/G_All_Stock_Num/2))
+	}
+	hard_code1 := uint32(270)
+	if round(a_2021_30/G_All_Stock_Num/2) == hard_code1 {
+		t.Logf("2017 can buy price { %dpe: %d/%d HKD }", round(pe_range_r), hard_code1*2, hard_code1)
+	} else {
+		t.Errorf("2017 can buy price { %dpe: 372 HKD, but:%d }", round(pe_range_r), round(a_2021_30/G_All_Stock_Num/2))
+	}
+
+	year_num := 4.0
+	t.Logf("公司20%%增长，收益增长率范围为%.1f%% ~ %.1f%%",
+		utils.GetRatio(G_My_Costing, 360, year_num),
+		utils.GetRatio(G_My_Costing, 540, year_num))
+}
+
+func Test_pe_v4(t *testing.T) {
 
 	// ###########################
 	// 715是2017年股东应占溢利
